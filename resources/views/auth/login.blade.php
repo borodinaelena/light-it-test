bb2788d4d7d1a8ad1af8c22213176db8ca890032
borodinaelena <borodina.elena.25@gmail.com>
2017-03-22 17:01:03 +0200

    Updated login template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,9 +2,13 @@
 
 @section('content')
 <div class="container">
+
     <div class="panel panel-default">
+    <h1 class="text-center">
+Войдите с Facebook, чтобы писать сообщения.
+</h1>
          <div class="panel-body">
-            <a href="redirect"  class="btn btn-primary" id="login_fb">FB Login</a>
+            <a href="redirect"  class="btn btn-primary" id="login_fb">Facebook Login</a>
          </div>           
      </div>
 </div>
